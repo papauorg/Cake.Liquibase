3433aeca9f2118ad1d554372b12c445ab411cae5
Philipp Grathwohl <philipp.grathwohl@papau.org>
2017-09-09 09:56:08 +0200

    Add update overload for handling liquibase options

--- src/Cake.Liquibase/Aliases/LiquibaseAliases.cs
+++ src/Cake.Liquibase/Aliases/LiquibaseAliases.cs
@@ -16,11 +16,30 @@ namespace Cake.Liquibase
         /// <summary>
         /// Executes liquibase using the 'update' cli parameter and the information in the liquibase settings.
         /// </summary>
+        /// <param name="liquibaseSettings">The liquibase settings and arguments for running liquibase.</param>
         /// <returns>Liquibase return code</returns>
         [CakeMethodAlias]
         public static int UpdateDatabase(this ICakeContext context, LiquibaseSettings liquibaseSettings)
         {
             return new LiquibaseRunner(context.ProcessRunner, context.Log, context.Tools).Start(Commands.Update, liquibaseSettings);
+        }
+
+        /// <summary>
+        /// Executes liquibase using the 'update' cli parameter and default liquibase settings that can be overwritten by using the action.
+        /// </summary>
+        /// <param name="liquibaseSettingsAction">An action that lets you modify the default liquibase settings.</param>
+        /// <returns>Liquibase return code</returns>
+        [CakeMethodAlias]
+        public static int UpdateDatabase(this ICakeContext context, Action<LiquibaseSettings> liquibaseSettingsAction)
+        {
+            var liquibaseSettings = new LiquibaseSettings();
+
+            if (liquibaseSettingsAction != null) 
+            {
+                liquibaseSettingsAction(liquibaseSettings);
+            }
+
+            return UpdateDatabase(context, liquibaseSettings);
         }
     }
 }
