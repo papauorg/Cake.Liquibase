3093ce519b09ee6f3ab8f2b1f6812e01131bcdba
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2021-12-02 16:18:11 +0100

    Update packages

--- src/Cake.Liquibase/Runner/LiquibaseRunner.cs
+++ src/Cake.Liquibase/Runner/LiquibaseRunner.cs
@@ -1,131 +1,133 @@
-using System;
-using System.Linq;
 using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 using Cake.Core.Tooling;
-using Cake.Liquibase;
 using Cake.Liquibase.Runner.LiquibaseCommands;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cake.Liquibase.Runner
 {
-    /// <summary>
-    /// Builds up the command line and executes the liquibase jar file.
-    /// </summary>
-    public class LiquibaseRunner
-    {
-        public ICakeLog Log { get; private set; }
-        public IProcessRunner ProcessRunner { get; private set; }
-        public IToolLocator Tools { get; private set; }
-        public IGlobber Globber {get; private set; }
-        public ICakePlatform Platform {get; private set;}
+	/// <summary>
+	/// Builds up the command line and executes the liquibase jar file.
+	/// </summary>
+	public class LiquibaseRunner
+	{
+		public ICakeLog Log { get; private set; }
+		public IProcessRunner ProcessRunner { get; private set; }
+		public IToolLocator Tools { get; private set; }
+		public IGlobber Globber { get; private set; }
+		public ICakePlatform Platform { get; private set; }
 
-        public LiquibaseRunner(IProcessRunner processRunner, ICakeLog log, IToolLocator tools, IGlobber globber, ICakePlatform platform)
-        {
-            ProcessRunner = processRunner ?? throw new ArgumentNullException(nameof(processRunner));
-            Log = log ?? throw new ArgumentNullException(nameof(log));
-            Tools = tools ?? throw new ArgumentNullException(nameof(tools));
-            Globber = globber ?? throw new ArgumentNullException(nameof(Globber));
-            Platform = platform ?? throw new ArgumentNullException(nameof(platform));
-        }
-        
-        /// <summary>
-        /// Runs liquibase against a database using the update parameter.
-        /// </summary>
-        /// <returns></returns>
-        public int Start(BaseCommand command, LiquibaseSettings settings)
-        {
-            if (command == null)
-                throw new ArgumentNullException("command");
+		public LiquibaseRunner(IProcessRunner processRunner, ICakeLog log, IToolLocator tools, IGlobber globber, ICakePlatform platform)
+		{
+			ProcessRunner = processRunner ?? throw new ArgumentNullException(nameof(processRunner));
+			Log = log ?? throw new ArgumentNullException(nameof(log));
+			Tools = tools ?? throw new ArgumentNullException(nameof(tools));
+			Globber = globber ?? throw new ArgumentNullException(nameof(globber));
+			Platform = platform ?? throw new ArgumentNullException(nameof(platform));
+		}
 
-            if (settings == null)
-                throw new ArgumentNullException("settings");
+		/// <summary>
+		/// Runs liquibase against a database using the update parameter.
+		/// </summary>
+		/// <returns></returns>
+		public int Start(BaseCommand command, LiquibaseSettings settings)
+		{
+			if (command == null)
+				throw new ArgumentNullException("command");
 
-            var liquibaseJar = ResolveLiquibaseJarFile(settings.LiquibaseJar);
-            if (liquibaseJar == null)
-                throw new ArgumentException($"Liquibase jar file not found under '{settings.LiquibaseJar}'");
-            
-            
-            var javaExecutable = GetJavaExecutable(settings); 
-                        
-            var arguments = new Helpers.ArgumentBuilder(command, settings, liquibaseJar, Globber).Build();
-            var processSettings = GetProcessSettings(arguments, settings);
-            
-            var process = this.ProcessRunner.Start(javaExecutable, processSettings);
-            process.WaitForExit();
-            var messages = process.GetStandardError();
-            var exitCode = process.GetExitCode();
+			if (settings == null)
+				throw new ArgumentNullException("settings");
 
-            RedirectErrorOutput(messages, exitCode);
+			var liquibaseJar = ResolveLiquibaseJarFile(settings.LiquibaseJar);
+			if (liquibaseJar == null)
+				throw new ArgumentException($"Liquibase jar file not found under '{settings.LiquibaseJar}'");
 
-            return exitCode;
-        }
 
-        private void RedirectErrorOutput(IEnumerable<string> messages, int exitCode)
-        {
-            LogLevel level = LogLevel.Information;
-            if (exitCode != 0) {
-                level = LogLevel.Error;
-            }
+			var javaExecutable = GetJavaExecutable(settings);
 
-            foreach (string message in messages)
-            {
-                Log.Write(Verbosity.Minimal, level, message, null);
-            }
-        }
+			var arguments = new Helpers.ArgumentBuilder(command, settings, liquibaseJar, Globber).Build();
+			var processSettings = GetProcessSettings(arguments, settings);
 
-        private FilePath GetJavaExecutable(LiquibaseSettings settings)
-        {
-            string executableToResolve = settings.JavaSettings.Executable;
-            if (string.IsNullOrEmpty(executableToResolve))
-            {
-                executableToResolve = Platform.IsUnix() ? "java" : "java.exe";
-            }
+			var process = this.ProcessRunner.Start(javaExecutable, processSettings);
+			process.WaitForExit();
+			var messages = process.GetStandardError();
+			var exitCode = process.GetExitCode();
 
-            var javaExecutable = Tools.Resolve(executableToResolve);
+			RedirectErrorOutput(messages, exitCode);
 
-            if (javaExecutable == null)
-                throw new ArgumentException($"The java executable could not be found under '{executableToResolve}'.");
+			return exitCode;
+		}
 
-            return javaExecutable;
-        }
+		private void RedirectErrorOutput(IEnumerable<string> messages, int exitCode)
+		{
+			LogLevel level = LogLevel.Information;
+			if (exitCode != 0)
+			{
+				level = LogLevel.Error;
+			}
 
-        private ProcessSettings GetProcessSettings(string arguments, LiquibaseSettings settings)
-        {
-            var processSettings = new ProcessSettings {
-                Arguments = arguments,
-                RedirectStandardError = true
-            };
+			foreach (string message in messages)
+			{
+				Log.Write(Verbosity.Minimal, level, message, null);
+			}
+		}
 
-            if (settings.WorkingDirectory != null)
-            {
-                processSettings.UseWorkingDirectory(settings.WorkingDirectory);
-            }
+		private FilePath GetJavaExecutable(LiquibaseSettings settings)
+		{
+			string executableToResolve = settings.JavaSettings.Executable;
+			if (string.IsNullOrEmpty(executableToResolve))
+			{
+				executableToResolve = Platform.IsUnix() ? "java" : "java.exe";
+			}
 
-            return processSettings;
-        }
+			var javaExecutable = Tools.Resolve(executableToResolve);
 
-        private string ResolveLiquibaseJarFile(string liquibaseJarPattern)
-        {
-            Path jarFile = null;
+			if (javaExecutable == null)
+				throw new ArgumentException($"The java executable could not be found under '{executableToResolve}'.");
 
-            try
-            {
-                jarFile = Tools.Resolve(liquibaseJarPattern);
-            } 
-            catch (ArgumentException)
-            {
-                // illegal characters in path (when using *)
-                // fall back to globber.
-            }
+			return javaExecutable;
+		}
 
-            if (jarFile == null) {
-                // try file globber
-                jarFile = Globber.GetFiles(liquibaseJarPattern).FirstOrDefault();
-            }
+		private ProcessSettings GetProcessSettings(string arguments, LiquibaseSettings settings)
+		{
+			var processSettings = new ProcessSettings
+			{
+				Arguments = arguments,
+				RedirectStandardError = true
+			};
 
-            return jarFile?.FullPath;
-        }
-    }
+			if (settings.WorkingDirectory != null)
+			{
+				processSettings.UseWorkingDirectory(settings.WorkingDirectory);
+			}
+
+			return processSettings;
+		}
+
+		private string ResolveLiquibaseJarFile(string liquibaseJarPattern)
+		{
+			Path jarFile = null;
+
+			try
+			{
+				jarFile = Tools.Resolve(liquibaseJarPattern);
+			}
+			catch (ArgumentException)
+			{
+				// illegal characters in path (when using *)
+				// fall back to globber.
+			}
+
+			if (jarFile == null)
+			{
+				// try file globber
+				jarFile = Globber.GetFiles(liquibaseJarPattern).FirstOrDefault();
+			}
+
+			return jarFile?.FullPath;
+		}
+	}
 }
